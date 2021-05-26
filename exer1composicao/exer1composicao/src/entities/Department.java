ffc00516e9594f495eb9d93f888546aae639c391
Rodrigo Lima <digo-pe@hotmail.com>
2021-05-26 00:07:59 -0300

    Add exercicios java 11 de composiçao

---
+++ exer1composicao/exer1composicao/src/entities/Department.java
@@ -0,0 +1,23 @@
+package entities;
+
+public class Department {
+
+	private String name;
+	
+	public Department() {
+		
+	}
+	
+	public Department(String name) {
+		this.name = name;
+	}
+	
+	public String GetName() {
+		return name;
+	}
+	
+	public void SetName(String name) {
+		this.name = name;
+	}
+
+}
